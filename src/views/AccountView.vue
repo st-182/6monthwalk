d38e4b6610f337c19472da17c3fb7be4fc40c5b1
Aristidas Jasudas <31894656+st-182@users.noreply.github.com>
2023-12-25 16:59:55 +0000

    new rout added

---
+++ src/views/AccountView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+const courses: string[] = ["Freshlife28","random1"];
+
+const el = ref()
+
+onMounted(() => {
+  el.value // <div>
+})
+</script>
+
+<template>
+  <main>
+    <p>Hi, this is your account!</p>
+    <div class=""></div>
+    registered from
+    started {{ courses[0] }}
+  </main>
+</template>
